ceee7aa94ae6a67e65cf76c65deeb0514262f976
4l3sh4 <aleshanoorsalim@gmail.com>
2026-02-04 17:32:11 +0800

    Added all of the classes into main.py.

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
-from datetime import datetime
+from datetime import datetime, timezone
+import uuid
 
 app = Flask(__name__)
 
@@ -22,25 +23,229 @@ login_manager.login_view = 'login'
 
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     full_name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     password = db.Column(db.String(50), nullable=False)
 
 class UserProfile(db.Model):
     user_id = db.Column(
-        db.Integer,
+        db.String(36),
         db.ForeignKey('user.id'),
         primary_key=True
     )
-
     contact_number = db.Column(db.String(15), nullable=False)
     role = db.Column(db.String(10), nullable=False)
     address = db.Column(db.String(100), nullable=False)
     emergency_contact_name = db.Column(db.String(80), nullable=False)
     emergency_contact_number = db.Column(db.String(15), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=lambda:datetime.now(timezone.utc), nullable=False)
     account_status = db.Column(db.String(15), nullable=False)
+
+class BankAccount(db.Model):
+    bank_account_number = db.Column(db.String(34), primary_key=True)
+    bank_name = db.Column(db.String(100), nullable=False)
+
+class Admin(db.Model):
+    admin_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    user_id = db.Column(
+        db.String(36),
+        db.ForeignKey('user.id')
+    )
+
+class HOD(db.Model):
+    hod_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    user_id = db.Column(
+        db.String(36),
+        db.ForeignKey('user.id')
+    )
+
+class Reviewer(db.Model):
+    reviewer_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    user_id = db.Column(
+        db.String(36),
+        db.ForeignKey('user.id')
+    )
+
+class Researcher(db.Model):
+    researcher_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    user_id = db.Column(
+        db.String(36),
+        db.ForeignKey('user.id')
+    )
+
+class Department(db.Model):
+    department_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    hod_id = db.Column(
+        db.String(36),
+        db.ForeignKey('hod.hod_id')
+    )
+    department_name = db.Column(db.String(50), nullable=False)
+    department_description = db.Column(db.String(500), nullable=False)
+
+class GrantScheme(db.Model):
+    scheme_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    admin_id = db.Column(
+        db.String(36),
+        db.ForeignKey('admin.admin_id'),
+    )
+    department_id = db.Column(
+        db.String(36),
+        db.ForeignKey('department.department_id'),
+    )
+    description = db.Column(db.String(500), nullable=False)
+    eligibiliity = db.Column(db.String(500), nullable=False)
+    open_date = db.Column(db.Date, nullable=False)
+    close_date = db.Column(db.Date, nullable=False)
+    max_budget = db.Column(db.Integer, nullable=False)
+    project_duration_limit = db.Column(db.Integer, nullable=False)
+    required_documents = db.Column(db.String(500), nullable=False)
+    reporting_requirements = db.Column(db.String(500), nullable=False)
+    scheme_status = db.Column(db.String(20), nullable=False)
+    created_at = db.Column(db.DateTime, default=lambda:datetime.now(timezone.utc), nullable=False)
+    updated_at = db.Column(db.DateTime, default=lambda:datetime.now(timezone.utc), onupdate=lambda:datetime.now(timezone.utc), nullable=False)
+
+class Proposal(db.Model):
+    proposal_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    scheme_id = db.Column(
+        db.String(36),
+        db.ForeignKey('grantscheme.scheme_id'),
+    )
+    researcher_id = db.Column(
+        db.String(36),
+        db.ForeignKey('researcher.researcher_id'),
+    )
+    project_title = db.Column(db.String(100), nullable=False)
+    abstract = db.Column(db.String(500), nullable=False)
+    methodology = db.Column(db.String(500), nullable=False)
+    requested_budget = db.Column(db.Integer, nullable=False)
+    submission_date = db.Column(db.DateTime, nullable=False)
+    proposal_status = db.Column(db.String(15), nullable=False)
+
+class Review(db.Model):
+    review_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    proposal_id = db.Column(
+        db.String(36),
+        db.ForeignKey('proposal.proposal_id'),
+    )
+    reviewer_id = db.Column(
+        db.String(36),
+        db.ForeignKey('reviewer.reviewer_id'),
+    )
+    review_date = db.Column(db.DateTime, nullable=False)
+    recommendation = db.Column(db.String(20), nullable=False)
+    feedback = db.Column(db.String(500), nullable=False)
+
+class ReviewersAssignment(db.Model):
+    assignment_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    proposal_id = db.Column(
+        db.String(36),
+        db.ForeignKey('proposal.proposal_id'),
+    )
+    reviewer_id = db.Column(
+        db.String(36),
+        db.ForeignKey('reviewer.reviewer_id'),
+    )
+    assigned_date = db.Column(db.DateTime, default=lambda:datetime.now(timezone.utc), nullable=False)
+    assignment_status = db.Column(db.String(15), nullable=False)
+
+class HODEndorsement(db.Model):
+    hod_endorsement_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    hod_id = db.Column(
+        db.String(36),
+        db.ForeignKey('hod.hod_id'),
+    )
+    proposal_id = db.Column(
+        db.String(36),
+        db.ForeignKey('proposal.proposal_id'),
+    )
+    decision = db.Column(db.String(20), nullable=False)
+    decision_date = db.Column(db.DateTime, default=lambda:datetime.now(timezone.utc), nullable=False)
+    remarks = db.Column(db.String(500), nullable=False)
+
+class FinalDecision(db.Model):
+    final_decision_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    proposal_id = db.Column(
+        db.String(36),
+        db.ForeignKey('proposal.proposal_id'),
+    )
+    admin_id = db.Column(
+        db.String(36),
+        db.ForeignKey('admin.admin_id'),
+    )
+    decision = db.Column(db.String(15), nullable=False)
+    decision_date = db.Column(db.DateTime, default=lambda:datetime.now(timezone.utc), nullable=False)
+
+class Project(db.Model):
+    project_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    proposal_id = db.Column(
+        db.String(36),
+        db.ForeignKey('proposal.proposal_id'),
+    )
+    researcher_id = db.Column(
+        db.String(36),
+        db.ForeignKey('researcher.researcher_id'),
+    )
+    scheme_id = db.Column(
+        db.String(36),
+        db.ForeignKey('grantscheme.scheme_id'),
+    )
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
+    project_status = db.Column(db.String(15), nullable=False)
+
+class FundingAllocation(db.Model):
+    allocation_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    admin_id = db.Column(
+        db.String(36),
+        db.ForeignKey('admin.admin_id'),
+    )
+    project_id = db.Column(
+        db.String(36),
+        db.ForeignKey('project.project_id'),
+    )
+    total_amount = db.Column(db.Integer, nullable=False)
+    equipment_amount = db.Column(db.Integer, nullable=False)
+    materials_amount = db.Column(db.Integer, nullable=False)
+    travel_amount = db.Column(db.Integer, nullable=False)
+    other_amount = db.Column(db.Integer, nullable=False)
+    allocation_date = db.Column(db.DateTime, default=lambda:datetime.now(timezone.utc), nullable=False)
+    allocation_status = db.Column(db.String(20), nullable=False)
+
+class ProgressReport(db.Model):
+    progress_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    project_id = db.Column(
+        db.String(36),
+        db.ForeignKey('project.project_id'),
+    )
+    researcher_id = db.Column(
+        db.String(36),
+        db.ForeignKey('researcher.researcher_id'),
+    )
+    hod_id = db.Column(
+        db.String(36),
+        db.ForeignKey('hod.hod_id'),
+    )
+    period_start_date = db.Column(db.Date, nullable=False)
+    period_end_date = db.Column(db.Date, nullable=False)
+    summary = db.Column(db.String(500), nullable=False)
+    milestones_achieved = db.Column(db.String(500), nullable=False)
+    challenges = db.Column(db.String(500), nullable=False)
+    resource_usage = db.Column(db.String(500), nullable=False)
+    submission_date = db.Column(db.DateTime, default=lambda:datetime.now(timezone.utc), nullable=False)
+    status = db.Column(db.String(50), nullable=False)
+    hod_comments = db.Column(db.String(500), nullable=False)
+
+class Notification(db.Model):
+    notification_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    user_id = db.Column(
+        db.String(36),
+        db.ForeignKey('user.id'),
+    )
+    message = db.Column(db.String(100), nullable=False)
+    notif_type = db.Column(db.String(50), nullable=False)
+    created_at = db.Column(db.DateTime, default=lambda:datetime.now(timezone.utc), nullable=False)
+    is_read = db.Column(db.Boolean, default=False, nullable=False)
 
 @login_manager.user_loader
 def load_user(user_id):
